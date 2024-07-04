4aef5d6283c3b501120684c063c3ee92713b9a84
Abbas Jr <109457597+MoAbbasid@users.noreply.github.com>
2024-07-04 02:49:55 +0000

    testing codespaces

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/hand.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Ok handsign";
 
 /**
  * Project list
@@ -31,10 +31,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Github profile🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Just my github profile, you can check it out and see what I've been up to.",
+    url: "https://github.com/MoAbbasid",
   },
   {
     title: "Web Development for Beginners",
